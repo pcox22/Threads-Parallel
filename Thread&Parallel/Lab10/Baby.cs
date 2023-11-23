922d7c383d85adda18a432cda830adf2ec3c0b5a
pcox22 <151768109+pcox22@users.noreply.github.com>
2023-11-22 22:49:37 -0500

    Add files via upload

---
+++ Thread&Parallel/Lab10/Baby.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab10
+{
+    class Baby
+    {
+        private int time;
+        private string name;
+        private static Random rand = new Random();
+
+
+        public Baby(string title)
+        {
+            name = title;
+            time = rand.Next(5000);
+        }
+
+        public void run()
+        {
+            try
+            {
+                Console.WriteLine("My name is " + name + ", and I am going to sleep for " + time + "ms.");
+                Thread.Sleep(time);
+                Console.WriteLine("My name is " + name + ", I am awake, feed me!!!");
+            }
+            catch (ThreadInterruptedException)
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ Thread&Parallel/Lab10/Lab10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab10
+{
+    class Lab10
+    {
+        static void Main(string[] args)
+        {
+            Baby b1 = new Baby("Noah");
+            Baby b2 = new Baby("Olivia");
+            Baby b3 = new Baby("Liam");
+            Baby b4 = new Baby("Emma");
+            Baby b5 = new Baby("Amelia");
+
+            Thread t1 = new Thread(b1.run);
+            Thread t2 = new Thread(b2.run);
+            Thread t3 = new Thread(b3.run);
+            Thread t4 = new Thread(b4.run);
+            Thread t5 = new Thread(b5.run);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+
+
+        }
+
+
+    }
+}
